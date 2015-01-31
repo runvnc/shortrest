321b0018ace56c77e4c9e327576ad753c5aadd18
Jason Livesay <ithkuil@gmail.com>
2015-01-31 22:20:47 +0000

    make it work like the README says it does

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var restify = require('restify');
 
-var server = restify.createServer({
-    name: 'shortrest',
-    version: '1.0.0'
-});
-server.use(restify.acceptParser(server.acceptable));
-server.use(restify.queryParser());
-server.use(restify.bodyParser());
-
-exports.server = server;
+module.exports = function(port) {
+  var self = this;
+  self.server = restify.createServer({
+      name: 'shortrest',
+      version: '1.0.0'
+  });
+  self.server.use(restify.acceptParser(server.acceptable));
+  self.server.use(restify.queryParser());
+  self.server.use(restify.bodyParser());
+  self.get = self.server.get.bind(self.server);
+  self.post = self.server.post.bind(self.server);
+  self.server.listen(port);
+  return self;
+}
