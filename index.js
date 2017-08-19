1875e8ee96157ae055417eb2ec03426bad71a37f
Jason Livesay <runvnc@gmail.com>
2017-08-19 04:28:34 +0100

    needed to update restify

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ var jwt = require('restify-jwt');
 global['jwt'] = jwt;
 
 module.exports = function(port) {
+  var self = {};
   global['jwt'] = jwt;
-  var self = this;
   self.server = restify.createServer({
       name: 'shortrest',
       version: '1.0.0'
   });
-  self.server.use(restify.acceptParser(server.acceptable));
-  self.server.use(restify.queryParser());
-  self.server.use(restify.bodyParser());
+  self.server.use(restify.plugins.acceptParser(self.server.acceptable));
+  self.server.use(restify.plugins.queryParser());
+  self.server.use(restify.plugins.bodyParser());
   self.get = self.server.get.bind(self.server);
   self.post = self.server.post.bind(self.server);
   self.server.listen(port);
